d4a4364810231c75817c85331f9716205e4d81c2
david <davidsanchezdiaz72@gmail.com>
2024-04-26 11:54:48 +0200

    Añadir clase Encuesta con constructor para establecer título y generar UUID único opcionalmente

--- src/Models/encuesta.php
+++ src/Models/encuesta.php
@@ -2,6 +2,29 @@
 
 namespace Lenovo\Encuestas\model;
 
-class Encuesta extends Database{
+use Lenovo\Encuestas\model\Database;
+use PDO;
+
+class Encuesta extends Database {
+
+    private string $uuid;
     
+    /**
+     * Constructor de la clase Encuesta.
+     * 
+     * @param string $title Título de la encuesta.
+     * @param bool $createUUID Indica si se debe crear un UUID único para la encuesta.
+     */
+    public function __construct(private string $title, $createUUID = true)
+    {
+        // Llama al constructor de la clase Database para establecer la conexión a la base de datos.
+        parent::__construct();
+
+        // Genera un UUID único para la encuesta si $createUUID es true.
+        if ($createUUID) {
+            $this->uuid = uniqid();
+        }
+        // Se asigna un UUID único a la encuesta.
+        $this->uuid = uniqid();
+    }
 }
